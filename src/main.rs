e63239e38b609e502845de14af9c46deb6822016
Lena <me@lena.place>
2023-09-07 13:09:42 +0200

    start measuring stuff

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,11 @@ mod physics;
 mod r#const;
 
 use embassy_executor::Spawner;
+use embassy_rp::gpio::{Input, Pull};
 use embassy_rp::peripherals::USB;
 use embassy_rp::usb::{Driver, InterruptHandler};
 use embassy_rp::bind_interrupts;
+use embassy_time::{Duration, Timer, Instant};
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
@@ -23,8 +25,32 @@ async fn logger_task(driver: Driver<'static, USB>) {
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-
     // Set up USB logging
+    Timer::after(Duration::from_secs(2)).await;
+    log::info!("Initializing USB logger...");
     let driver = Driver::new(p.USB, Irqs);
     spawner.spawn(logger_task(driver)).unwrap();
+    log::info!("Initialized USB logger.");
+
+    let mut north = Input::new(p.PIN_14, Pull::Down);
+    let mut south = Input::new(p.PIN_15, Pull::Down);
+
+    let mut current_pole = false;
+    let mut last_time = Instant::now();
+    loop {
+        current_pole = !current_pole;
+        if current_pole {
+            north.wait_for_rising_edge().await;
+        } else {
+            south.wait_for_rising_edge().await;
+        }
+        let current_time = Instant::now();
+        let difference_time = current_time - last_time;
+        last_time = current_time;
+
+        let rpm = 15000.0 / difference_time.as_millis() as f32;
+
+        log::info!("A quarter of a turn happened! RPM: {}", rpm);
+        // a quarter of a turn
+    }
 }
